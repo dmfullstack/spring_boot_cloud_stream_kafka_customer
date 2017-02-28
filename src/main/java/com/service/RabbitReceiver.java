bb81bd9b1844bda1c10b1a275841b53f511adf8b
zhangsulei <youtong82@163.com>
2017-02-28 09:57:15 +0800

    1

--- src/main/java/com/service/RabbitReceiver.java
+++ src/main/java/com/service/RabbitReceiver.java
@@ -2,6 +2,7 @@ package com.service;
 
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 import com.Barista;
@@ -11,9 +12,17 @@ import com.domain.Product;
 public class RabbitReceiver {
 
     @StreamListener(Barista.INPUT_CHANNEL)
-    public void receiver(Message<Product> message){
+	//对于那些有返回数据的方法，必须使用@SendTo注解来指定返回数据的输出绑定目标。
+  	@SendTo(Barista.NETSHOES_PRODUCTS)
+    public Product receiver(Message<Product> message){
     	Product obj = message.getPayload();
         System.out.println("接受对象:" + obj);
+        return obj;
     }
+
+    @StreamListener(Barista.NETSHOES_PRODUCTS)
+	public void netshoesProducts(Product product) {
+    	System.out.println("接受对象2:" + product);
+	}
 
 }
