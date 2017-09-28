2ed170cf4990decb3fc0841b81aff1921e53af56
yhb2010 <youtong82@163.com>
2017-09-28 14:34:22 +0800

    1

--- src/main/java/com/service/RabbitReceiver.java
+++ src/main/java/com/service/RabbitReceiver.java
@@ -11,19 +11,15 @@ import com.domain.Product;
 @Component
 public class RabbitReceiver {
 
+	//定义在方法上，作用是将被修饰的方法注册为消息中间件上数据流的事件监听器，注解中的属性值对应了监听的消息通道名。
     @StreamListener(Barista.INPUT_CHANNEL)
-	//对于那些有返回数据的方法，必须使用@SendTo注解来指定返回数据的输出绑定目标。
-  	@SendTo(Barista.NETSHOES_PRODUCTS)
+	//很多时候在处理完输入消息后，需要反馈一个消息给对方，这时可以通过@SendTo注解来指定返回内容的输出通道
+  	@SendTo(Barista.OUTPUT_CHANNEL)
     public Product receiver(Message<Product> message){
     	Product obj = message.getPayload();
         System.out.println("接受对象:" + obj);
+        obj.setSku("2");
         return obj;
     }
-
-    //定义在方法上，作用是将被修饰的方法注册为消息中间件上数据流的事件监听器，注解中的属性值对应了监听的消息通道名。
-    @StreamListener(Barista.NETSHOES_PRODUCTS)
-	public void netshoesProducts(Product product) {
-    	System.out.println("接受对象2:" + product);
-	}
 
 }
