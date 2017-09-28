2272e984cfb6bbe44348f69dccd27d49cc62e299
yhb2010 <youtong82@163.com>
2017-09-28 09:35:14 +0800

    1

--- src/main/java/com/service/RabbitReceiver.java
+++ src/main/java/com/service/RabbitReceiver.java
@@ -20,6 +20,7 @@ public class RabbitReceiver {
         return obj;
     }
 
+    //定义在方法上，作用是将被修饰的方法注册为消息中间件上数据流的事件监听器，注解中的属性值对应了监听的消息通道名。
     @StreamListener(Barista.NETSHOES_PRODUCTS)
 	public void netshoesProducts(Product product) {
     	System.out.println("接受对象2:" + product);
